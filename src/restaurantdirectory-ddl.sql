9f3894cf4a6f7926b23bfd7e4ca8a26c6d9f9a8a
ESConstancia <99902972+ESConstancia@users.noreply.github.com>
2023-11-23 01:00:24 -0500

    Update restaurantdirectory-ddl.sql

--- src/restaurantdirectory-ddl.sql
+++ src/restaurantdirectory-ddl.sql
@@ -1,6 +1,20 @@
-# Creating basic tables for DB
+DROP TABLE IF EXISTS dietary_preference;
+CREATE TABLE dietary_preference (
+    dietary_preference_id int not null auto_increment,
+    dietary_preference_name varchar(256) not null,
+    PRIMARY KEY (dietary_preference_id)
+);
 
-drop table if exists restaurants;
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+    location_id int not null auto_increment,
+    location_address varchar(256) not null,
+    location_direction_from_VCU varchar(32),
+    location_neighborhood varchar(256),
+    PRIMARY KEY (location_id)
+);
+
+DROP TABLE IF EXISTS restaurants;
 CREATE TABLE restaurants (
     restaurant_id int not null auto_increment,
     restaurant_name varchar(256) not null,
@@ -9,37 +23,20 @@ CREATE TABLE restaurants (
     restaurant_location_id int,
     restaurant_type varchar(256),
     PRIMARY KEY (restaurant_id)
-)
+);
 
-drop table if exists review;
+DROP TABLE IF EXISTS review;
 CREATE TABLE review (
     review_id int not null auto_increment,
-    review_restaurant_id not null,
+    review_restaurant_id int not null,
     review_rating float not null,
-    review_text varchar (1026),
+    review_text varchar(1026),
     review_date date,
-    PRIMARY KEY (review_id)
-)
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (review_restaurant_id) REFERENCES restaurants(restaurant_id)
+);
 
-drop table if exists locations;
-CREATE TABLE locations (
-    location_id int not null auto_increment,
-    location_address varchar(256) not null,
-    location_direction_from_VCU varchar(32),
-    location_neighborhood varchar(256),
-    PRIMARY KEY (location_id)
-)
-
-drop table if exists discount;
-CREATE TABLE discount (
-    discount_id not null auto_increment,
-    discount_restaurant_id not null,
-    discount_description varchar(256),
-    discount_conditions varchar(256),
-    PRIMARY KEY (discount_id)
-)
-
-drop table if exists accessibility;
+DROP TABLE IF EXISTS accessibility;
 CREATE TABLE accessibility (
     accessibility_id int not null auto_increment,
     accessibility_restaurant_id int not null,
@@ -47,18 +44,17 @@ CREATE TABLE accessibility (
     accessibility_nearby_bus_stops varchar(256),
     accessibility_notes varchar(256),
     PRIMARY KEY (accessibility_id)
-)
+);
 
-drop table if exists menu;
+DROP TABLE IF EXISTS menu;
 CREATE TABLE menu (
     menu_id int not null auto_increment,
-    menu_restaurant_id int not null,
-    menu_item_id int not null,
-    PRIMARY KEY menu_id
-)
+    restaurant_id int not null,
+    PRIMARY KEY (menu_id),
+    FOREIGN KEY (restaurant_id) REFERENCES restaurants(restaurant_id)
+);
 
-
-drop table if exists item;
+DROP TABLE IF EXISTS item;
 CREATE TABLE item (
     item_id int not null auto_increment,
     item_dietary_preference_id int,
@@ -66,13 +62,16 @@ CREATE TABLE item (
     item_description varchar(512),
     item_price float not null,
     PRIMARY KEY (item_id)
-)
+);
 
-drop table if exists dietary_preference;
-CREATE TABLE dietary_preference (
-    dietary_preference_id int not null auto_increment,
-    dietary_preference_name varchar(256) not null,
-    PRIMARY KEY (dietary_preference_id)
-)
+DROP TABLE IF EXISTS menu_items;
+CREATE TABLE menu_items (
+    menu_item_id int not null auto_increment,
+    menu_id int not null,
+    item_id int not null,
+    PRIMARY KEY (menu_item_id),
+    FOREIGN KEY (menu_id) REFERENCES menu(menu_id),
+    FOREIGN KEY (item_id) REFERENCES item(item_id)
+);
 
 
