5546b9f032e7a1603ca2c984e0a6ccd4000a492b
Alex Welk <axwk68@gmail.com>
2023-12-03 15:16:35 -0500

    Created and setup basic API file

---
+++ src/project-flask.py
@@ -0,0 +1,83 @@
+from flask import Flask, jsonify, requests
+import os
+import sys
+import pandas as pd
+from tabulate import tabulate
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError, ProgrammingError
+from IPython.display import display, Markdown
+
+# make sure to add PROJECT_DB_NAME to .env file
+config_map = {
+    'user':'CMSC508_USER',
+    'password':'CMSC508_PASSWORD',
+    'host':'CMSC508_HOST',
+    'database':'PROJECT_DB_NAME'
+}
+# load and store credentials
+load_dotenv()
+config = {}
+for key in config_map.keys():
+    config[key] = os.getenv(config_map[key])
+flag = False
+for param in config.keys():
+    if config[param] is None:
+        flag = True
+        print(f"Missing {config_map[param]} in .env file")
+
+engine_uri = f"mysql+pymysql://{config['user']}:{config['password']}@{config['host']}/{config['database']}"
+
+# create a database connection.  THIS IS THE ACTUAL CONNECTION!
+try:
+    conn = create_engine(engine_uri)
+except ArgumentError as e:
+    print(f"create_engine: Argument Error: {e}")
+    #sys.exit(1)
+except NoSuchModuleError as e:
+    print(f"create_engine: No Such Module Error: {e}")
+    #sys.exit(1)
+except Exception as e:
+    print(f"create_engine: An error occurred: {e}")
+    #sys.exit(1)
+    
+def my_sql_wrapper( query ):
+    """ takes a query and returns a pandas dataframe for output """
+    try:
+        df = pd.read_sql( query, conn )
+    except Exception as e:
+        df = pd.DataFrame({'Query error': ["(see error message above)"]})
+        msg = str(e).replace(") (",")\n(")
+        print(msg)
+    return df
+
+def my_sql_statement( statement ):
+    """ used with DDL, when the statement doesn't return any results. """
+    try:
+        with conn.begin() as x:
+            x.execute(text(statement))
+            x.commit()
+    #    conn = create_engine(engine_uri)
+        result = ""
+    except Exception as e:
+        result = f"Error: {str(e)}"
+#    conn = create_engine(engine_uri)
+    return result
+
+app = Flask(__name__)
+
+@app.route('/api/tables')
+def api_show_tables():
+    df = my_sql_wrapper("show tables")
+    json_data = df.to_json()
+    return jsonify(json_data)
+
+@app.route('/api/restaurants')
+def api_show_restaurants():
+    df = my_sql_wrapper("select * from restaurants")
+    json_data = df.to_json()
+    return jsonify(json_data)
+
+if __name__ == '__main__':
+    print(my_sql_wrapper("show tables"))
+    app.run(debug=True)
