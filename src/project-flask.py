b850dad056298385e6dc0bb80da34f300af88763
ESConstancia <99902972+ESConstancia@users.noreply.github.com>
2023-12-05 22:31:28 -0500

    Update project-flask.py
    working on post for restaurants kinda confused

--- src/project-flask.py
+++ src/project-flask.py
@@ -73,12 +73,36 @@ def api_show_tables():
     json_data = df.to_json()
     return jsonify(json_data)
 
-@app.route('/api/restaurants')
-def api_show_restaurants():
-    full_string = ('select * from restaurants')
-    df = my_sql_wrapper(full_string)
-    json_data = df.to_json()
-    return jsonify(json_data)
+@app.route('/api/restaurants', methods=["GET", "POST"])
+def restaurants():
+    if request.method == "GET":
+        full_string = 'SELECT * FROM restaurants'
+        df = my_sql_wrapper(full_string)
+        
+        restaurants = []
+        for index, row in df.iterrows():
+            restaurant = OrderedDict([
+                ('restaurant_id', row['restaurant_id']),
+                ('restaurant_name', row['restaurant_name']),
+                ('restaurant_formality_level', row['restaurant_formality_level']),
+                ('restaurant_avg_total_per_person', row['restaurant_avg_total_per_person']),
+                ('restaurant_location_id', row['restaurant_location_id']),
+                ('restaurant_type', row['restaurant_type'])
+            ])
+            restaurants.append(restaurant)
+
+        json_string = json.dumps(restaurants)
+        return jsonify(json.loads(json_string))
+    
+    if request.method == "POST":
+        restaurant = {
+            'restaurant_id': request.form.get('restaurant_id'),
+            'restaurant_name': request.form.get('restaurant_name'),
+            'restaurant_formality_level': request.form.get('restaurant_formality_level'),
+            'restaurant_avg_total_per_person': request.form.get('restaurant_avg_total_per_person'),
+            'restaurant_location_id': request.form.get('restaurant_location_id'),
+            'restaurant_type': request.form.get('restaurant_type')
+        }
 
 @app.route('/api/restaurants/<ID>')
 def api_show_specific_restaurants(ID):
